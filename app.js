e82b8165f1fb668ad2786bd7d766076b210167de
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2016-01-08 21:38:30 -0500

    Finish assignment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     engines = require('consolidate'),
     bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
     app = express();
 
 app.engine('html', engines.nunjucks);
@@ -15,23 +17,42 @@ function errorHandler(err, req, res, next) {
   res.render('error_template', { error: err });
 }
 
-app.get('/', function(req, res, next) {
-  res.render('fruitPicker', { fruits: ['apple', 'banana', 'orange', 'peach'] });
-});
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+  assert.equal(null, err);
+  console.log("Successfully connected to MongoDB.");
 
-app.post('/', function(req, res, next) {
-  var fruit = req.body.fruit;
-  if (typeof fruit === 'undefined') {
-    next(Error('Please choose a fruit!'));
-  } else {
-    res.status(200).send('Your favorite fruit is ' + fruit);
-  }
-});
+  app.get('/', function(req, res, next) {
+    res.redirect('/movies');
+  });
 
-app.use(errorHandler);
+  app.get('/movies', function(req, res, next) {
+    db.collection('movies').find({}).toArray(function(err, docs) {
+      res.render('movies', { 'movies': docs } );
+    });
+  });
 
-var server = app.listen(3000, function() {
-  var port = server.address().port;
-  console.log('Express server listening on port %s', port);
+  app.post('/movies', function(req, res, next) {
+    var title = req.body.title,
+        year = req.body.year,
+        imdb = req.body.imdb;
+
+    if (title === '' || year === '' || imdb === '') {
+      next(Error('You must specify a title, year, and imdb'));
+    } else {
+      db.collection('movies').insertOne({title: title, year: year, imdb: imdb}, function(err, r) {
+        assert.equal(null, err);
+        assert.equal(1, r.insertedCount);
+        res.redirect('/movies');
+      });
+    }
+
+  });
+
+  app.use(errorHandler);
+
+  var server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log('Express server listening on port %s', port);
+  });
 });
 
